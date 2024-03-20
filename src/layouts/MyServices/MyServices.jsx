ae2b36635705391e5589fd70bcf15f75fe3468aa
AlexisKcc8 <CodeKJS863@gmail.com>
2024-03-20 08:27:24 -0600

    add animation in section services for each element when scrolling

--- src/layouts/MyServices/MyServices.jsx
+++ src/layouts/MyServices/MyServices.jsx
@@ -1,7 +1,8 @@
-import { CardService } from "@components/CardService/CardService";
-import { dataServices } from "@data/services";
+import { CardService } from '@components/CardService/CardService';
+import { dataServices } from '@data/services';
 
-import "./stylesMyServices.scss";
+import './stylesMyServices.scss';
+import { InView } from 'react-intersection-observer';
 export const MyServices = () => {
   const getSpecificService = (services, indexService) => {
     indexService = parseInt(indexService);
@@ -12,47 +13,103 @@ export const MyServices = () => {
       id="services"
       className="container-fluid-services container-fluid min-vh-100 "
     >
-      <article className="container-services container">
+      <article className="container-services container overflow-hidden">
         <header className="row-header row text-center ">
-          <p className="subtitle-section m-0">Te ofrecemos siempre</p>
-          <h2 className="title-section m-0 p-0">
-            <strong>Servicios confiables y de la mejor calidad.</strong>
-          </h2>
+          <InView>
+            {({ inView, ref }) => (
+              <p
+                ref={ref}
+                className={`subtitle-section m-0 animate__animated ${
+                  inView ? 'animate__fadeInDown' : 'animate__fadeOut'
+                }`}
+              >
+                Te ofrecemos siempre
+              </p>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <h2
+                ref={ref}
+                className={`title-section m-0 p-0 animate__animated ${
+                  inView ? 'animate__fadeInDown' : 'animate__fadeOut'
+                }`}
+              >
+                <strong>Servicios confiables y de la mejor calidad.</strong>
+              </h2>
+            )}
+          </InView>
         </header>
 
         <section className="row-grid-services row  ">
-          <article className="col-12 p-0  mb-3 mb-lg-0">
-            <CardService
-              data={{
-                service: getSpecificService(dataServices, 7),
-              }}
-            />
-          </article>
-          <article className="col-12 col-lg-6 p-0 mb-3 mb-lg-0">
-            <CardService
-              data={{
-                bgColor: "#1b544f",
-                service: getSpecificService(dataServices, 1),
-              }}
-            />
-          </article>
-          <article className="col-12 col-lg-6 p-0 mb-3 mb-lg-0">
-            <CardService
-              data={{
-                bgColor: "#1c6961",
-                service: getSpecificService(dataServices, 2),
-              }}
-            />
-          </article>
-          <article className="col-12 p-0 mb-3 mb-lg-0">
-            <CardService
-              data={{
-                orderWiewInfo: [2, 1],
+          <InView>
+            {({ inView, ref }) => (
+              <article
+                ref={ref}
+                className={`col-12 p-0  mb-3 mb-lg-0 animate__animated ${
+                  inView ? 'animate__fadeInLeft' : 'animate__fadeOutRight'
+                }`}
+              >
+                <CardService
+                  data={{
+                    service: getSpecificService(dataServices, 7),
+                  }}
+                />
+              </article>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <article
+                ref={ref}
+                className={`col-12 col-lg-6 p-0 mb-3 mb-lg-0 animate__animated ${
+                  inView ? 'animate__fadeInLeft' : 'animate__fadeOutRight'
+                }`}
+              >
+                <CardService
+                  data={{
+                    bgColor: '#1b544f',
+                    service: getSpecificService(dataServices, 1),
+                  }}
+                />
+              </article>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <article
+                ref={ref}
+                className={`col-12 col-lg-6 p-0 mb-3 mb-lg-0 animate__animated ${
+                  inView ? 'animate__fadeInRight' : 'animate__fadeOutLeft'
+                }`}
+              >
+                <CardService
+                  data={{
+                    bgColor: '#1c6961',
+                    service: getSpecificService(dataServices, 2),
+                  }}
+                />
+              </article>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <article
+                ref={ref}
+                className={`col-12 p-0 mb-3 mb-lg-0 animate__animated ${
+                  inView ? 'animate__fadeInRight' : 'animate__fadeOutLeft'
+                }`}
+              >
+                <CardService
+                  data={{
+                    orderWiewInfo: [2, 1],
 
-                service: getSpecificService(dataServices, 6),
-              }}
-            />
-          </article>
+                    service: getSpecificService(dataServices, 6),
+                  }}
+                />
+              </article>
+            )}
+          </InView>
         </section>
       </article>
     </section>
