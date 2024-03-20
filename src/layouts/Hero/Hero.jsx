01cb214cf1d17974e909256de9237620efdfe0bd
AlexisKcc8 <CodeKJS863@gmail.com>
2024-03-19 18:39:25 -0600

    delete libary react-intersection-observer

--- src/layouts/Hero/Hero.jsx
+++ src/layouts/Hero/Hero.jsx
@@ -1,14 +1,6 @@
-import { InView } from "react-intersection-observer";
-
-import "./stylesHero.scss";
+import './stylesHero.scss';
 
 export const Hero = () => {
-  const options = {
-    threshold: 0,
-    triggerOnce: false,
-    initialInView: true,
-  };
-
   return (
     <section
       className="container-fluid-hero container-fluid overflow-hidden"
@@ -19,73 +11,26 @@ export const Hero = () => {
           <section className="container py-0 min-vh-100 ">
             <div className="row min-vh-100">
               <div className="col-12 container-content ">
-                <InView {...options}>
-                  {({ ref, inView }) => (
-                    <figure
-                      ref={ref}
-                      className={`figure-logo m-0 mt-5 animate__animated ${
-                        inView ? "animate__fadeIn " : "animate__fadeOutLeft"
-                      }`}
-                    >
-                      <img
-                        className="m-0"
-                        src="/imgs/logo-family-medical.png"
-                        alt="Logo-family-medical"
-                      />
-                    </figure>
-                  )}
-                </InView>
-                <InView {...options}>
-                  {({ ref, inView }) => (
-                    <h2
-                      ref={ref}
-                      className={`title-brand animate__animated ${
-                        inView ? "animate__fadeIn " : "animate__fadeOutLeft"
-                      }`}
-                    >
-                      <strong>
-                        <span>Family</span>Medical
-                      </strong>
-                    </h2>
-                  )}
-                </InView>
-                <InView {...options}>
-                  {({ ref, inView }) => (
-                    <h2
-                      ref={ref}
-                      className={`title animate__animated ${
-                        inView ? "animate__fadeInDown " : "animate__fadeOutLeft"
-                      }`}
-                    >
-                      Dr. ADRIAN FUENTES
-                    </h2>
-                  )}
-                </InView>
-                <InView {...options}>
-                  {({ ref, inView }) => (
-                    <h3
-                      ref={ref}
-                      className={`subtitle animate__animated ${
-                        inView ? "animate__fadeIn " : "animate__fadeOutLeft"
-                      }`}
-                    >
-                      <strong>MÉDICO CIRUJANO</strong>
-                    </h3>
-                  )}
-                </InView>
-                <InView {...options}>
-                  {({ ref, inView }) => (
-                    <p
-                      ref={ref}
-                      className={`description animate__animated ${
-                        inView ? "animate__fadeIn " : "animate__fadeOutLeft"
-                      }`}
-                    >
-                      "Su familia, nuestra prioridad: atención médica de calidad
-                      centrada en usted."
-                    </p>
-                  )}
-                </InView>
+                <figure className={`figure-logo m-0 mt-5 animate__animated`}>
+                  <img
+                    className="m-0"
+                    src="/imgs/logo-family-medical.png"
+                    alt="Logo-family-medical"
+                  />
+                </figure>
+                <h2 className={`title-brand `}>
+                  <strong>
+                    <span>Family</span>Medical
+                  </strong>
+                </h2>
+                <h2 className={`title `}>Dr. ADRIAN FUENTES</h2>
+                <h3 className={`subtitle `}>
+                  <strong>MÉDICO CIRUJANO</strong>
+                </h3>
+                <p className={`description`}>
+                  "Su familia, nuestra prioridad: atención médica de calidad
+                  centrada en usted."
+                </p>
               </div>
             </div>
           </section>
