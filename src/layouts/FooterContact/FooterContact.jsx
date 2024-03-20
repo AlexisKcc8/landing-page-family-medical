4d387cccf7a33a7c7fe95444c3717d5313ff1077
AlexisKcc8 <CodeKJS863@gmail.com>
2024-03-20 14:53:41 -0600

    add animation in elemets sections footer and prices

--- src/layouts/FooterContact/FooterContact.jsx
+++ src/layouts/FooterContact/FooterContact.jsx
@@ -1,70 +1,185 @@
-import { ItemsSocialMedia } from "@components/ItemsSocialMedia/ItemsSocialMedia";
+import { InView } from 'react-intersection-observer';
+import { ItemsSocialMedia } from '@components/ItemsSocialMedia/ItemsSocialMedia';
 
-import "./stylesFooterContact.scss";
+import './stylesFooterContact.scss';
 export const FooterContact = () => {
   return (
     <footer
       id="contact"
       className="container-fluid-footer-contact container-fluid "
     >
-      <section className="container-footer-contact container  px-lg-5">
+      <section className="container-footer-contact container  px-lg-5 overflow-hidden">
         <article className="row-footer-contact row d-flex">
           <div className="col-contact col-12 order-2 order-lg-1  col-lg-3 ">
-            <h4 className="title-contact m-0">Contacto</h4>
-            <p className="description-contact">
-              No dudes en ponerte en contacto para comenzar este viaje juntos
-              hacia tu bienestar integral. ¡Tu salud es mi prioridad!"
-            </p>
-            <ul className="list-social-media">
-              <ItemsSocialMedia showTitle={true} />
-            </ul>
+            <InView>
+              {({ inView, ref }) => (
+                <h4
+                  ref={ref}
+                  className={`title-contact m-0 animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  Contacto
+                </h4>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <p
+                  ref={ref}
+                  className={`description-contact animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  No dudes en ponerte en contacto para comenzar este viaje
+                  juntos hacia tu bienestar integral. ¡Tu salud es mi prioridad!
+                </p>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <ul
+                  ref={ref}
+                  className={`list-social-media animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  <ItemsSocialMedia showTitle={true} />
+                </ul>
+              )}
+            </InView>
           </div>
 
           <div className="col-brand col-12 order-1 col-lg-6 px-lg-5">
-            <picture className="picture-img-brand m-0 p-0">
-              <img
-                className="img-brand m-0 p-0"
-                src="/imgs/logo-family-medical.png"
-                alt=""
-              />
-            </picture>
-
-            <h5 className="title-brand m-0 p-0">
-              <span>Family</span>Medical
-            </h5>
-            <p className="description-brand">
-              La pasión por mi trabajo impulsa cada paso de mi práctica,
-              guiándome hacia una atención centrada en el paciente que va más
-              allá del simple tratamiento de enfermedades.
-            </p>
-            <h4 className="subtitle-text">Estamos para servirles!</h4>
+            <InView>
+              {({ inView, ref }) => (
+                <picture
+                  ref={ref}
+                  className={`picture-img-brand m-0 p-0 animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  <img
+                    className="img-brand m-0 p-0"
+                    src="/imgs/logo-family-medical.png"
+                    alt=""
+                  />
+                </picture>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <h5
+                  ref={ref}
+                  className={`title-brand m-0 p-0 animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  <span>Family</span>Medical
+                </h5>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <p
+                  ref={ref}
+                  className={`description-brand animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  La pasión por mi trabajo impulsa cada paso de mi práctica,
+                  guiándome hacia una atención centrada en el paciente que va
+                  más allá del simple tratamiento de enfermedades.
+                </p>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <h4
+                  ref={ref}
+                  className={`subtitle-text animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  Estamos para servirles!
+                </h4>
+              )}
+            </InView>
           </div>
 
           <div className="col-schedules col-12 order-3  col-lg-3 ">
-            <h4 className="title-schedules m-0">Nuestros Horarios</h4>
-            <p className="description-schedules">
-              "Consulta nuestros horarios de atención médica familiar para
-              reservar citas y planificar visitas. Prioriza la salud de tu
-              familia con facilidad."
-            </p>
-            <ul className="list-schedules m-0 p-0">
-              <li className="item-shedules">
-                <p className="w-50 text-start">Lunes a viernes</p>
-                <p className="w-50 text-end">4:00 pm - 10:00 pm</p>
-              </li>
-              <li className="item-shedules">
-                <p className="w-50 text-start">Sábados y domingos</p>
-                <p className="w-50 text-end">
-                  8:00 am - 10:00 pm (cita previa)
+            <InView>
+              {({ inView, ref }) => (
+                <h4
+                  ref={ref}
+                  className={`title-schedules m-0 animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  Nuestros Horarios
+                </h4>
+              )}
+            </InView>
+            <InView>
+              {({ inView, ref }) => (
+                <p
+                  ref={ref}
+                  className={`description-schedules animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  &#34;Consulta nuestros horarios de atención médica familiar
+                  para reservar citas y planificar visitas. Prioriza la salud de
+                  tu familia con facilidad.&rdquo;
                 </p>
-              </li>
+              )}
+            </InView>
+
+            <ul className="list-schedules m-0 p-0">
+              <InView>
+                {({ inView, ref }) => (
+                  <li
+                    ref={ref}
+                    className={`item-shedules animate__animated ${
+                      inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                    }`}
+                  >
+                    <p className="w-50 text-start">Lunes a viernes</p>
+                    <p className="w-50 text-end">4:00 pm - 10:00 pm</p>
+                  </li>
+                )}
+              </InView>
+              <InView>
+                {({ inView, ref }) => (
+                  <li
+                    ref={ref}
+                    className={`item-shedules animate__animated ${
+                      inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                    }`}
+                  >
+                    <p className="w-50 text-start">Sábados y domingos</p>
+                    <p className="w-50 text-end">
+                      8:00 am - 10:00 pm (cita previa)
+                    </p>
+                  </li>
+                )}
+              </InView>
             </ul>
           </div>
           <div className="col-copyright col-12 order-4 d-flex justify-content-center aling-items-center p-0">
-            <p className="m-0 mt-4 message-copyright">
-              Copyright © 2024 <strong>FamilyMedical</strong>. Todos los
-              derechos reservados. by <strong>Adrian Fuentes</strong>.
-            </p>
+            <InView>
+              {({ inView, ref }) => (
+                <p
+                  ref={ref}
+                  className={`m-0 mt-4 message-copyright animate__animated ${
+                    inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                  }`}
+                >
+                  Copyright © 2024 <strong>FamilyMedical</strong>. Todos los
+                  derechos reservados. by <strong>Adrian Fuentes</strong>.
+                </p>
+              )}
+            </InView>
           </div>
         </article>
       </section>
--- src/layouts/Prices/Prices.jsx
+++ src/layouts/Prices/Prices.jsx
@@ -1,6 +1,7 @@
-import { CardPrice } from "@components/CardPrice/CardPrice";
-import { dataServices } from "@data/services";
-import "./stylesPrices.scss";
+import { CardPrice } from '@components/CardPrice/CardPrice';
+import { dataServices } from '@data/services';
+import './stylesPrices.scss';
+import { InView } from 'react-intersection-observer';
 export const Prices = () => {
   const filterServicesByPackage = (services, idPackage) => {
     idPackage = idPackage.toString();
@@ -14,52 +15,97 @@ export const Prices = () => {
     >
       <article className="container-prices container">
         <header className="row-header row text-center">
-          <p className="m-0 subtitle-section">
-            Elaboramos nuevos paquetes para que puedas
-          </p>
-          <h2 className="title-section m-0 p-0">
-            <strong>Aprovechar nuestros nuevos precios.</strong>
-          </h2>
+          <InView>
+            {({ inView, ref }) => (
+              <p
+                ref={ref}
+                className={`m-0 subtitle-section animate__animated ${
+                  inView ? 'animate__fadeInDown' : 'animate__fadeOut'
+                }`}
+              >
+                Elaboramos nuevos paquetes para que puedas
+              </p>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <h2
+                ref={ref}
+                className={`title-section m-0 p-0 animate__animated ${
+                  inView ? 'animate__fadeInDown' : 'animate__fadeOut'
+                }`}
+              >
+                <strong>Aprovechar nuestros nuevos precios.</strong>
+              </h2>
+            )}
+          </InView>
         </header>
 
         <section className="row-cards row mt-3 ">
-          <div className="col-12 col-md-6 col-lg-4 mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center">
-            <CardPrice
-              data={{
-                title: "Paquete 1: Asesoría médica",
-                description:
-                  "Ideal para tener acceso a orientación médica de calidad a un precio asequible. Garantizamos respuestas claras y precisas para sus inquietudes de salud.",
-                price: "$80.00",
-                services: filterServicesByPackage(dataServices, 1),
-                packageBelongs: "1",
-              }}
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center">
-            <CardPrice
-              data={{
-                title: "Paquete 3: Consulta a domicilio",
-                description:
-                  "Creado para proporcionarle atención médica de calidad sin la molestia de desplazarse a una clínica u hospital. Estaremos encantados de visitarlo en la comodidad de su hogar.",
-                isDark: true,
-                price: "Personalizado",
-                services: filterServicesByPackage(dataServices, 3),
-                packageBelongs: "3",
-              }}
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4  mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center">
-            <CardPrice
-              data={{
-                title: "Paquete 2: Consulta médica",
-                description:
-                  "Diseñado con el objetivo de brindarle la atención médica que necesita para sentirse seguro y respaldado en cada paso de su viaje hacia la salud óptima.",
-                price: "$300.00",
-                services: filterServicesByPackage(dataServices, 2),
-                packageBelongs: "2",
-              }}
-            />
-          </div>
+          <InView>
+            {({ inView, ref }) => (
+              <div
+                ref={ref}
+                className={`col-12 col-md-6 col-lg-4 mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center animate__animated ${
+                  inView ? 'animate__slideInLeft' : 'animate__fadeOut'
+                }`}
+              >
+                <CardPrice
+                  data={{
+                    title: 'Paquete 1: Asesoría médica',
+                    description:
+                      'Ideal para tener acceso a orientación médica de calidad a un precio asequible. Garantizamos respuestas claras y precisas para sus inquietudes de salud.',
+                    price: '$80.00',
+                    services: filterServicesByPackage(dataServices, 1),
+                    packageBelongs: '1',
+                  }}
+                />
+              </div>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <div
+                ref={ref}
+                className={`col-12 col-md-6 col-lg-4 mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center animate__animated ${
+                  inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                }`}
+              >
+                <CardPrice
+                  data={{
+                    title: 'Paquete 3: Consulta a domicilio',
+                    description:
+                      'Creado para proporcionarle atención médica de calidad sin la molestia de desplazarse a una clínica u hospital. Estaremos encantados de visitarlo en la comodidad de su hogar.',
+                    isDark: true,
+                    price: 'Personalizado',
+                    services: filterServicesByPackage(dataServices, 3),
+                    packageBelongs: '3',
+                  }}
+                />
+              </div>
+            )}
+          </InView>
+          <InView>
+            {({ inView, ref }) => (
+              <div
+                ref={ref}
+                className={`col-12 col-md-6 col-lg-4  mb-3 p-0 d-md-flex justify-content-md-center align-items-md-center animate__animated ${
+                  inView ? 'animate__fadeInRight' : 'animate__fadeOut'
+                }`}
+              >
+                <CardPrice
+                  data={{
+                    title: 'Paquete 2: Consulta médica',
+                    description:
+                      'Diseñado con el objetivo de brindarle la atención médica que necesita para sentirse seguro y respaldado en cada paso de su viaje hacia la salud óptima.',
+                    price: '$300.00',
+                    services: filterServicesByPackage(dataServices, 2),
+                    packageBelongs: '2',
+                  }}
+                />
+              </div>
+            )}
+          </InView>
         </section>
       </article>
     </section>
