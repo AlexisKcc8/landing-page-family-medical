afeb33d29c0ada3c21c804c4fa7e04d392b632ac
AlexisKcc8 <CodeKJS863@gmail.com>
2024-04-12 19:44:29 -0600

    effect in intersection observer just one in component hero

--- src/layouts/Hero/Hero.jsx
+++ src/layouts/Hero/Hero.jsx
@@ -1,8 +1,12 @@
-import { InView } from 'react-intersection-observer';
-import { TextLogo } from '@components/TextLogo/TextLogo';
+import { InView } from "react-intersection-observer";
+import { TextLogo } from "@components/TextLogo/TextLogo";
 
-import './stylesHero.scss';
+import "./stylesHero.scss";
 export const Hero = () => {
+  const options = {
+    threshold: 0,
+    triggerOnce: true,
+  };
   return (
     <section className="container-fluid-hero container-fluid" id="home">
       <article className="container-hero container py-0 m-0">
@@ -10,12 +14,12 @@ export const Hero = () => {
           <section className="container py-0 min-vh-100 ">
             <div className="row min-vh-100">
               <div className="col-12 container-content ">
-                <InView>
+                <InView {...options}>
                   {({ inView, ref }) => (
                     <picture
                       ref={ref}
                       className={`picture-logo m-0 mt-5 animate__animated ${
-                        inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                        inView ? "animate__fadeIn" : "animate__fadeOut"
                       }`}
                     >
                       <source
@@ -36,35 +40,35 @@ export const Hero = () => {
                     </picture>
                   )}
                 </InView>
-                <InView>
+                <InView {...options}>
                   {({ inView, ref }) => (
                     <div ref={ref}>
                       <TextLogo
                         classTextLogo={`title-brand animate__animated ${
-                          inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                          inView ? "animate__fadeIn" : "animate__fadeOut"
                         }`}
                       />
                     </div>
                   )}
                 </InView>
-                <InView>
+                <InView {...options}>
                   {({ inView, ref }) => (
                     <h2
                       ref={ref}
                       className={`name-doctor animate__animated ${
-                        inView ? 'animate__fadeInDown' : 'animate__fadeOut'
+                        inView ? "animate__fadeInDown" : "animate__fadeOut"
                       }`}
                     >
                       Dr. CARLOS FUENTES
                     </h2>
                   )}
                 </InView>
-                <InView>
+                <InView {...options}>
                   {({ inView, ref }) => (
                     <h1
                       ref={ref}
                       className={`subtitle animate__animated ${
-                        inView ? 'animate__fadeIn' : 'animate__fadeOut'
+                        inView ? "animate__fadeIn" : "animate__fadeOut"
                       }`}
                     >
                       <strong>MÉDICO CIRUJANO / MÉDICO FAMILIAR</strong>
